0657aafeefb4d408e2ee9aeb6a5ab4d86c296dfd
jspencerpittman <jspencerpittman@gmail.com>
2024-03-28 14:13:31 -0500

    Created static form page

--- frontend/wikiwormhole/src/App.js
+++ frontend/wikiwormhole/src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import './font.css';
 import gsap from "gsap";
 import { useGSAP } from '@gsap/react';
-import Home from './home/home.js';
+import Form from './form/form.js';
 
 gsap.registerPlugin(useGSAP);
 
 function App() {
   return (
     <div className="content">
-      <Home/>
+      <Form/>
     </div>
   );
 }
